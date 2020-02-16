362251bd9f222d9b07522929385db8ca56ceb669
Shreyas Y. Bhise <syb29@kill.cs.rutgers.edu>
2020-02-16 18:08:35 -0500

    something

--- fileSort.c
+++ fileSort.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+typedef struct Node {
+	void* value;
+	struct Node* next;
+
+};
 
 int main(int argc, char** argv) {
+	
 
 
-
-
-
-
-    return 0;
+	return 0;
 }
