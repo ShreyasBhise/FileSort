89d849900a2a6589cc67a7ea14e11368c9e7cf18
Ryan A. Jensen <raj107@ilab3.cs.rutgers.edu>
2020-02-20 19:41:03 -0500

    yes

--- fileSort.c
+++ fileSort.c
@@ -15,9 +15,15 @@ int main(int argc, char** argv) {
     }
     if(strlen(argv[1]) == 2 && argv[1][0] == '-') {
         if(argv[1][1] != 'q' && argv[1][1] != 'i' ) {
-            printf("invalid sorting flag, expected -i or -q\n);
+            printf("invalid sorting flag, expected -i or -q\n");
             exit(0);
         }
     }
+	int fd = open(argv[2], O_RDONLY);
+	if(fd==-1){
+		printf("");
+		exit(0);
+	}	
+	printf("Hello world\n");
 	return 0;
 }
